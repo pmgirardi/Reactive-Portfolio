7126641cd7efbcc872662f1d5aebfed489902722
pmgirardi <117805446+pmgirardi@users.noreply.github.com>
2023-05-11 21:42:41 -0700

    added spacing to aboutme content

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -3,11 +3,16 @@ import avatar from '/Users/waterscorpio/UCBootCamp/Assignments/Reactive-Portfoli
 
 export default function AboutMe() {
   return (
-	<div className="container py-4 px-4">
+	<div className="container">
  	<h1 className="text-center">Paige Girardi.</h1> 
 		<img src= {avatar} className="rounded float-end" alt="portfolioimage"></img>
 		<p>
-		My experience in tech started while attending Arizona State University for my BS in Geography. My coursework included Geographic Information Technology, learning mapping software and data manipulation. I learned abou the real-world collection of data and how it is converted and represented digitally through my experience in the creation of the City of Hughson's GIS Database as a GIS Mapping and Data Collection intern. I found my strength in problem-solving and finding unique solutions through my experience in pilot projects. This is were I focused my interest, gaining experience in map editing and digitization for companies such as Apple (Apple Maps Application) and Amazon (Delivery Tracking). Currently, I live in Grizzly Flats, CA and I am attending UC Berkley Coding Bootcamp, earning a certificate in Full Stack Web Development. </p>
+		My experience in tech started while attending Arizona State University for my BS in Geography.</p> 
+		<p>My coursework included Geographic Information Technology, learning mapping software and data manipulation.</p> 
+		<p>I learned about the real-world collection of data and how it is converted and represented digitally through my experience in the creation of the City of Hughson's GIS Database as a GIS Mapping and Data Collection intern. </p>
+		
+		<p>I found my strength in problem-solving and finding unique solutions through my experience in pilot projects.</p>
+		 <p>This is were I focused my interest, gaining experience in map editing and digitization for companies such as Apple (Apple Maps Application) and Amazon (Delivery Tracking).</p> <p>Currently, I live in Grizzly Flats, CA and I am attending UC Berkley Coding Bootcamp, earning a certificate in Full Stack Web Development. </p>
 
 </div>
   )
