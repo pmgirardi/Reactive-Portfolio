7b6e994d9f6af9ada5a758ed46330ab4ba5c8fab
pmgirardi <117805446+pmgirardi@users.noreply.github.com>
2023-05-23 22:52:06 -0700

    imported resume pdf so it is downloadable and viewable

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import resumepdf from "../../images/Paige-Girardi-Resume-2023.pdf";
+
 
 
 export default function Resume() {
@@ -7,9 +9,9 @@ export default function Resume() {
  <h3 className="text-center">TECHNICAL SKILLS</h3>
 <p>JavaScript ES6+, CSS3, HTML5, SQL, NoSQL, GitHub, MongoDB, MySQL, Express, React, Node, Handlebars, jQuery, Bootstrap, GIS Proprietary Software, Data Collection, Digitization, Archival Processing, Archival Standards, Records Management, QGIS, Apple Map Application Software, FME, ArcGIS, OpenStreetMap, Survey 123, Microsoft: SharePoint, Word, Excel, and PowerPoint.
 </p>
-<a href="https://docs.google.com/document/d/1-AEQyGNUeFt8IBn8a_QdrG7jV4j35elfGjuC-ileh_8" class="btn btn-light btn-outline-dark btn-lg">View Resume</a>
+<a href= {resumepdf} class="btn btn-light btn-outline-dark btn-lg">View Resume</a>
 
-<a href="src/images/Web Development _ Full Stack Resume Sample.pdf" download="Web Development _ Full Stack Resume Sample.pdf">
+<a href= {resumepdf} download="Paige-Girardi-Resume-2023.pdf">
     <button className="btn btn-dark btn-outline-secondary btn-lg">Download File</button>
 </a>
 
