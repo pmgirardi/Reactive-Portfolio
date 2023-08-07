971c2c9a7f7ad4f315984bc6886dca1c1bf8b2cf
pmgirardi <117805446+pmgirardi@users.noreply.github.com>
2023-08-07 15:07:25 -0700

    updated portfolio projects

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import Book from '/Users/waterscorpio/UCBootCamp/Assignments/Reactive-Portfolio/src/images/BookSearch.png'
 import Text from '/Users/waterscorpio/UCBootCamp/Assignments/Reactive-Portfolio/src/images/TextEditor.png';
-import Note from '/Users/waterscorpio/UCBootCamp/Assignments/Reactive-Portfolio/src/images/NoteTaker.png';
 import Workday from '/Users/waterscorpio/UCBootCamp/Assignments/Reactive-Portfolio/src/images/Work Day Scheduler.gif';
-import fantasticBeasts from '/Users/waterscorpio/UCBootCamp/Assignments/Reactive-Portfolio/src/images/Fantastic-Beasts.png'
+import pythonNews from '/Users/waterscorpio/UCBootCamp/Assignments/Reactive-Portfolio/src/images/python.png'
 import CardGroup from 'react-bootstrap/CardGroup';
 import Card from 'react-bootstrap/Card';
 
@@ -15,31 +13,18 @@ export default function Portfolio() {
 
           <CardGroup>
           <Card>
-        <Card.Img variant="top" src= {fantasticBeasts} />
+        <Card.Img variant="top" src= {pythonNews} />
         <Card.Body>
-          <Card.Title>Group Project: MERN Built Application for Selling Mystical Creatures</Card.Title>
+          <Card.Title>Python News Feed</Card.Title>
           <Card.Text>
-          A shopping application that allows customers to explore and purchase a wide range of mythical creatures.
+         Python refactored app, originally built using Node.js. The app, Just Tech News, lets users submit links to tech-related articles, comment on other users' articles, and upvote articles.
           </Card.Text>
         </Card.Body>
         <Card.Footer>
-        <Card.Link rel="noopener noreferrer" href="https://fantastic-beasts.herokuapp.com/" class="card-link">Deployed App </Card.Link>
-        <Card.Link rel="noopener noreferrer" href="https://github.com/ikaera/Dream-Project" class="card-link">Github</Card.Link>
+        <Card.Link rel="noopener noreferrer" href="https://desolate-mountain-13116-927d36e5fa8b.herokuapp.com//" class="card-link">Deployed App </Card.Link>
+        <Card.Link rel="noopener noreferrer" href="https://github.com/pmgirardi/python-newsfeed" class="card-link">Github</Card.Link>
           </Card.Footer>
           </Card>
-          <Card>
-        <Card.Img variant="top" src= {Book} /> 
-        <Card.Body>
-          <Card.Title>Book Search App </Card.Title>
-          <Card.Text>
-          A fully functioning Google Books API search engine built with a RESTful API, refactored to be a GraphQL API built with Apollo Server.
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-        <Card.Link rel="noopener noreferrer" href="https://waterhorse.herokuapp.com/" class="card-link">Heroku </Card.Link>
-        <Card.Link rel="noopener noreferrer" href="https://github.com/pmgirardi/Book-Search" class="card-link">GitHub</Card.Link>
-          </Card.Footer>
-      </Card>
       <Card>
         <Card.Img variant="top" src= {Text} /> 
         <Card.Body>
@@ -53,19 +38,6 @@ export default function Portfolio() {
         <Card.Footer>
         <Card.Link rel="noopener noreferrer" href="https://whispering-falls-80138.herokuapp.com/" class="card-link">Heroku </Card.Link>
         <Card.Link rel="noopener noreferrer" href="https://github.com/pmgirardi/PWA-Text-Editor" class="card-link">GitHub</Card.Link>
-          </Card.Footer>
-      </Card>
-      <Card>
-        <Card.Img variant="top" src= {Note} /> 
-        <Card.Body>
-          <Card.Title>Note Taker App </Card.Title>
-          <Card.Text>
-          An application that can be used to write and save notes. This application uses an Express.js back end and will save and retrieve note data from a JSON file. 
-          </Card.Text>
-        </Card.Body>
-        <Card.Footer>
-        <Card.Link rel="noopener noreferrer" href="https://the-note-taker-by-pg.herokuapp.com/" class="card-link">Heroku </Card.Link>
-        <Card.Link rel="noopener noreferrer" href="https://github.com/pmgirardi/The-Note-Taker" class="card-link">GitHub</Card.Link>
           </Card.Footer>
       </Card>
       <Card>
